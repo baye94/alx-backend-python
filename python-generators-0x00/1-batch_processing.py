12ab062d919e309a687b4cbde73b4dd6a3f86c37
Baye Cheikh FALL <fallcheikh@finetechgroupe.com>
2025-05-16 13:51:43 +0000

    correction

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -8,7 +8,7 @@ def stream_users_in_batches(batch_size):
         database="ALX_prodev"
     )
     cursor = connection.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM user_data;")
+    cursor.execute("SELECT * FROM user;")
 
     batch = []
     for row in cursor:
@@ -26,6 +26,5 @@ def stream_users_in_batches(batch_size):
 def batch_processing(batch_size):
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
-            if user['age'] > 25:
-                print(user)
-    print("Batch processing completed.")
+            if user["age"] > 25:
+                yield user 
