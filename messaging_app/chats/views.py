7fd75cba8a756d5d9bbf91a135dce188ff734d0e
Baye Cheikh FALL <fallcheikh@finetechgroupe.com>
2025-06-05 19:19:30 +0000

    view

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,16 +1,33 @@
 # chats/views.py
 
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
-from .models import Message
+from rest_framework.response import Response
+from .models import Message, Conversation
 from .serializers import MessageSerializer
 from .permissions import IsParticipantOfConversation
 from rest_framework.permissions import IsAuthenticated
 
 class MessageViewSet(ModelViewSet):
-    queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
 
     def get_queryset(self):
-        # Only return messages from conversations the user participates in
         return Message.objects.filter(conversation__participants=self.request.user)
+
+    def perform_create(self, serializer):
+        conversation_id = self.request.data.get('conversation')
+
+        if not conversation_id:
+            return Response({'error': 'conversation_id is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            conversation = Conversation.objects.get(id=conversation_id)
+        except Conversation.DoesNotExist:
+            return Response({'error': 'Conversation not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        if self.request.user not in conversation.participants.all():
+            return Response({'error': 'You are not a participant in this conversation'},
+                            status=status.HTTP_403_FORBIDDEN)
+
+        serializer.save(sender=self.request.user, conversation=conversation)
