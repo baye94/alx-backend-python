ee3932bc0318db3c6f14bf7f9a89149a69abb5a1
Baye Cheikh FALL <fallcheikh@finetechgroupe.com>
2025-06-12 21:37:53 +0000

    view

---
+++ Django-signals_orm-0x04/messaging/Views.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+
+@login_required
+def delete_user(request):
+    user = request.user
+    logout(request)
+    user.delete()  # Triggers post_delete signal
+    return redirect('home')  # Or any landing page after deletion
