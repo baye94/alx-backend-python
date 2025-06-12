e0deeebef0ec839eba796e22d5907f9691b3f162
Baye Cheikh FALL <fallcheikh@finetechgroupe.com>
2025-06-12 21:30:21 +0000

    model

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -6,6 +6,8 @@ class Message(models.Model):
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
+    edited_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='edited_messages')
 
     def __str__(self):
         return f'From {self.sender} to {self.receiver}: {self.content[:20]}'
@@ -15,8 +17,7 @@ class Notification(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     read = models.BooleanField(default=False)
-    edited = models.BooleanField(default=False) 
-
+    edited = models.BooleanField(default=False)
 
     def __str__(self):
         return f'Notification for {self.user.username} - Message ID {self.message.id}'
@@ -25,6 +26,7 @@ class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
     old_content = models.TextField()
     edited_at = models.DateTimeField(auto_now_add=True)
+    edited_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'Edit of message {self.message.id} at {self.edited_at}'
